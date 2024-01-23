8c187d9aa14422f02e2604c852feb4948bc49ba2
wkopec5 <kopec.will@gmail.com>
2024-01-23 05:31:32 -0600

    Added Project
    TO-DO:
    ------------
    - Finish Readme File
    - Comment Code

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "4d97d6dbd0f74990bcc4c4aaf549b8fc"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
